e1b3447627aa3c50258eaa354f1e571a3f0591ce
Yohvnn <yohann.chan-chew-hong@edu.dsti.institute>
2026-04-06 22:12:07 +0200

    fix: :bug: vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -25,5 +25,10 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
     css: true,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html', 'lcov'],
+      reportsDirectory: './coverage',
+    },
   },
 })
